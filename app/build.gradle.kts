3a287b8f6b26e01829d4056a760522d9e500d632
polaralias <76123410+polaralias@users.noreply.github.com>
2026-05-25 19:40:43 +0100

    chore: release 1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.polaralias.coupio"
         minSdk = 29
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.0.1"
+        versionCode = 2
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
